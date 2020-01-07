934444a189a41766adfc8d61ab1d7228ef95f9cb
DarkWeb <54339318+TheDarkW3b@users.noreply.github.com>
2020-01-07 09:20:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://cross007.herokuapp.com/' + config.TOKEN)
+    bot.set_webhook(url='https://listprom.herokuapp.com/' + config.TOKEN)
     return "!", 200
 
 
